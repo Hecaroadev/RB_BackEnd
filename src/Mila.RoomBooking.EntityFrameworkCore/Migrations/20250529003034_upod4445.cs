cadddbdb37b8363c3772921f9dc27520a2d2eacb
Yousef Alqahtani <yalqahtani@rqm.sa>
2025-05-29 05:27:28 +0300

    sde

---
+++ src/Mila.RoomBooking.EntityFrameworkCore/Migrations/20250529003034_upod4445.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mila.RoomBooking.Migrations
+{
+    /// <inheritdoc />
+    public partial class upod4445 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookingRequests_TimeSlots_TimeSlotId",
+                table: "BookingRequests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_TimeSlots_TimeSlotId",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_TimeSlotId",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BookingRequests_TimeSlotId",
+                table: "BookingRequests");
+
+            migrationBuilder.DropColumn(
+                name: "TimeSlotId",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "TimeSlotId",
+                table: "BookingRequests");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "TimeSlotId",
+                table: "Bookings",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TimeSlotId",
+                table: "BookingRequests",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_TimeSlotId",
+                table: "Bookings",
+                column: "TimeSlotId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookingRequests_TimeSlotId",
+                table: "BookingRequests",
+                column: "TimeSlotId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookingRequests_TimeSlots_TimeSlotId",
+                table: "BookingRequests",
+                column: "TimeSlotId",
+                principalTable: "TimeSlots",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_TimeSlots_TimeSlotId",
+                table: "Bookings",
+                column: "TimeSlotId",
+                principalTable: "TimeSlots",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
